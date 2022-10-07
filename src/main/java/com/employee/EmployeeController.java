d50cb99b4ff680bd16dff22db0eb41251c6d0e3a
homozapien <bakau.onafuwa@softgineer.com>
2022-10-07 14:14:48 -0500

    Downgrade runtime from JDk11 to JDK8; simple code refactor

--- src/main/java/com/employee/EmployeeController.java
+++ src/main/java/com/employee/EmployeeController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,15 @@ public class EmployeeController
 	 
 	@Autowired
 	EmployeeService service;
+	
+	@GetMapping(value="/")
+	public String returnSimpleMsg() 
+	{
+		String message = "Welcome to CI/CD Deployment of a Simple SpringBoot Application; demonstrating \n"
+				       + "integration between Git, Docker, Jenkins (on EC2), and AWS EC2 VM!";
+		
+		return  message;
+	}
 	
 	@RequestMapping(value = "/findAll", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
 	public List<Employee> getAllEmployees() 
